3ff24f4cfee50fe77bf96ec0fc61684491114273
tauquirahmed <tauquirahmed9@gmail.com>
2024-02-21 23:57:39 +0530

    face-recognition

---
+++ fr.py
@@ -0,0 +1,66 @@
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration, VideoTransformerBase
+import av
+import cv2
+import streamlit as st
+from faceDetection import MPFaceDetection
+from faceNet.faceNet import FaceNet
+
+
+facenet = FaceNet(
+    detector=MPFaceDetection(),
+    onnx_model_path="models/faceNet.onnx",
+    anchors="faces",
+    force_cpu=True,
+)
+
+
+recognize = False
+
+capture = False
+
+if "count" not in st.session_state:
+    st.session_state.count = 0
+
+name = ""
+
+st.header("Face Recognition Attendance System")
+
+
+with st.sidebar:
+    st.title("Menu: ")
+    st.success("First turn on the webcam, then do any one of the following tasks.")
+
+    name = st.text_input("Enter the Name of the Employee")
+    if st.toggle("Capture & Store Image"):
+        if name:
+            capture = True
+            st.write("Hi " + name + " your Image has been Captured")
+            st.session_state.count += 1
+    else:
+        capture = False
+
+    st.warning("If you want to use Face Recognition, first disable the Capture Image")
+
+    if st.toggle("Start Face Recognition"):
+        recognize = True
+    else:
+        recognize = False
+
+
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    if recognize:
+        img = facenet(img)
+
+    if capture:
+        facenet.detect_save_faces(img, name=name)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(
+    key="key",
+    video_frame_callback=callback,
+    media_stream_constraints={"video": True, "audio": False},
+)
---
+++ main.py
@@ -0,0 +1,24 @@
+# main.py
+from utils import FPSmetric
+from engine import Engine
+from faceDetection import MPFaceDetection
+from faceNet.faceNet import FaceNet
+
+if __name__ == '__main__':
+    facenet = FaceNet(
+        detector = MPFaceDetection(),
+        onnx_model_path = "models/faceNet.onnx", 
+        anchors = "faces",
+        force_cpu = True,
+    )
+    engine = Engine(webcam_id=0, show=True, custom_objects=[facenet, FPSmetric()])
+    # engine = Engine(webcam_id=0, show=True, custom_objects=[facenet])
+
+
+    # save first face crop as anchor, otherwise don't use
+    while not facenet.detect_save_faces(engine.process_webcam(return_frame=True), output_dir="faces"):
+        continue
+
+
+
+    engine.run()
